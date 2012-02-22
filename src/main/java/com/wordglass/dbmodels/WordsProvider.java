31fc625d314c5fbab9f12624cb71ea6410203b7b
labuser <labuser@deb6-64.Natick>
2012-02-22 18:42:01 -0500

    add log file config

--- src/main/java/com/wordglass/dbmodels/WordsProvider.java
+++ src/main/java/com/wordglass/dbmodels/WordsProvider.java
@@ -3,12 +3,17 @@ package com.wordglass.dbmodels;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.wordglass.db.DBAccess;
 
 public class WordsProvider {
+
+	private static Logger logger = LoggerFactory.getLogger(WordsProvider.class);
 
 	/**
 	 * Return the lists of words
@@ -22,8 +27,11 @@ public class WordsProvider {
 		List<String> words = new ArrayList<String>();
 		DBCursor cursor = coll.find();
 
+		//logger.info("Found " + cursor.length() + " words");
+
 		while (cursor.hasNext()) {
 			DBObject l = cursor.next();
+			logger.info(l.toString());
 			words.add((String) l.get("word"));
 		}
 		return words;
